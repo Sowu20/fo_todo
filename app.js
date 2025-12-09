b6deb2392a0d7b11a35f5f655027c104b5cca856
Sowu20 <sowukelly67@gmail.com>
2025-12-09 17:32:38 +0000

    to_do

--- app.js
+++ app.js
@@ -18,10 +18,8 @@ app.use((req, res, next) => {
 
 const usersRoute = require('./routes/user');
 const taskRoute = require('./routes/task');
-const hrllo = require('./routes/hello');
 
 app.use('/api/user', usersRoute);
 app.use('/api/task', taskRoute);
-app.use(hrllo);
 
 module.exports = app;
--- routes/hello.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', () => {
-    console.log('Hello World');
-})
-
-module.exports = router;
